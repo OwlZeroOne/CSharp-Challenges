0af2b2b0b4bc3904d48c02033a84df96e5ac3d97
Mateusz <mat.pasternak06@gmail.com>
2023-05-13 13:42:19 +0100

    Weather Station - Refactoring Conditionals

---
+++ Exercism/Conditionals/WeatherStation.cs
@@ -0,0 +1,124 @@
+namespace Challenges.Exercism.Conditionals;
+
+public class WeatherStation
+{
+    private Reading reading;
+    private List<DateTime> recordDates = new List<DateTime>();
+    private List<decimal> temperatures = new List<decimal>();
+
+    public void AcceptReading(Reading reading)
+    {
+        this.reading = reading;
+        recordDates.Add(DateTime.Now);
+        temperatures.Add(reading.Temperature);
+    }
+
+    public void ClearAll()
+    {
+        reading = new Reading();
+        recordDates.Clear();
+        temperatures.Clear();
+    }
+
+    public decimal LatestTemperature => reading.Temperature;
+
+    public decimal LatestPressure => reading.Pressure;
+
+    public decimal LatestRainfall => reading.Rainfall;
+
+    public bool HasHistory => recordDates.Count > 1;
+
+    public Outlook ShortTermOutlook
+    {
+        get
+        {
+            if (reading.Equals(new Reading())) throw new ArgumentException();
+
+            switch (reading.Temperature)
+            {
+                case < 30m when reading.Pressure < 10m:
+                    return Outlook.Cool;
+                
+                case > 50:
+                    return Outlook.Good;
+                
+                default:
+                    return Outlook.Warm;
+            }
+        }
+    }
+
+    public Outlook LongTermOutlook
+    {
+        get
+        {
+            switch (reading.WindDirection)
+            {
+                case WindDirection.Southerly:
+                case WindDirection.Easterly when reading.Temperature > 20:
+                    return Outlook.Good;
+                
+                case WindDirection.Northerly:
+                    return Outlook.Cool;
+                
+                case WindDirection.Easterly when reading.Temperature <= 20:
+                    return Outlook.Warm;
+                
+                case WindDirection.Westerly:
+                    return Outlook.Rainy;
+                
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+
+    public State RunSelfTest()
+    {
+        return reading.Equals(new Reading()) ? State.Bad : State.Good;
+    }
+}
+
+/*** Please do not modify this struct ***/
+public struct Reading
+{
+    public decimal Temperature { get; }
+    public decimal Pressure { get; }
+    public decimal Rainfall { get; }
+    public WindDirection WindDirection { get; }
+
+    public Reading(decimal temperature, decimal pressure,
+        decimal rainfall, WindDirection windDirection)
+    {
+        Temperature = temperature;
+        Pressure = pressure;
+        Rainfall = rainfall;
+        WindDirection = windDirection;
+    }
+}
+
+/*** Please do not modify this enum ***/
+public enum State
+{
+    Good,
+    Bad
+}
+
+/*** Please do not modify this enum ***/
+public enum Outlook
+{
+    Cool,
+    Rainy,
+    Warm,
+    Good
+}
+
+/*** Please do not modify this enum ***/
+public enum WindDirection
+{
+    Unknown, // default
+    Northerly,
+    Easterly,
+    Southerly,
+    Westerly
+}
